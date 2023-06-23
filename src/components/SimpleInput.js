017ed249803ff166cceee6e48087ed7d3b10b7a0
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-06-23 11:26:10 +0530

    add basic user input validation using states

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,30 +2,55 @@ import { useState, useRef } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
+  const [inputIsValid, setInputISValid] = useState(false);
+  const [inputFieldTouched, setInputFieldTouched] = useState(false);
   const inputRef = useRef();
 
   const setinputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
 
+  const inputBlurHandler = () => {
+    setInputFieldTouched(true);
+  };
+
   const onSubmitHandler = (event) => {
     event.preventDefault();
+
+    setInputFieldTouched(true);
+
+    if (enteredName.trim() === "") {
+      setInputISValid(false);
+      return;
+    }
+
+    setInputISValid(true);
     console.log(inputRef.current.value);
     console.log(enteredName);
     setEnteredName("");
   };
 
+  const inputDataInvalid = !inputIsValid && inputFieldTouched;
+
+  const inputStyles = inputDataInvalid
+    ? "form-control invalid"
+    : "form-control";
+
   return (
     <form onSubmit={onSubmitHandler}>
-      <div className="form-control">
+      <div className={inputStyles}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={inputRef}
           onChange={setinputChangeHandler}
           type="text"
           id="name"
+          onBlur={inputBlurHandler}
           value={enteredName}
         />
+        {inputDataInvalid && (
+          <p className="error-text">Input must not be empty!!!</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
