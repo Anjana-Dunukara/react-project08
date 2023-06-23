a28d41c9dff7e4147aff3930fe790553decc0cc9
Anjana Dunukara <anjanaheshanthach@gmail.com>
2023-06-23 10:46:25 +0530

    using useState and useRef to get input data

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,31 @@
+import { useState, useRef } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const inputRef = useRef();
+
+  const setinputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(inputRef.current.value);
+    console.log(enteredName);
+    setEnteredName("");
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={onSubmitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={inputRef}
+          onChange={setinputChangeHandler}
+          type="text"
+          id="name"
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
